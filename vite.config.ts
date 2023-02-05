c26fe4275cdbab097a94533593fa1da6dc5a3594
Script Raccoon <scriptraccoon@gmail.com>
2023-02-05 08:34:28 +0100

    bring back vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import type { UserConfig } from "vite";
 
 const config: UserConfig = {
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		{
+			name: "socket.io plugin",
+			configureServer(server) {
+				console.log("I am injected into the vite dev server");
+			},
+		},
+	],
 };
 
 export default config;
